0ee338da56724ec776dba764fee0747a92e36657
Li-Kevin <h16861h@gmail.com>
2016-02-16 14:33:33 +0800

    revise model Product...

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	belongs_to :store
+	
+	#has_many :product_option_typeships
+	#has_many :option_types, :through => product_option_typeships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216040921) do
+ActiveRecord::Schema.define(version: 20160216061448) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "store_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string   "name"
